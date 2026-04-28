e5e09004326609386bd39156fa5564057f02b8c4
StressedBread <martinpolak02@gmail.com>
2026-04-28 21:11:51 +0200

    feat: add FlashcardsController and basic data query class

---
+++ StressedBread.Flashcards/StressedBread.Flashcards/Data/Queries/FlashcardsQueries.cs
@@ -0,0 +1,8 @@
+﻿namespace StressedBread.Flashcards.Data.Queries;
+internal class FlashcardsQueries
+{
+    internal string GetFlashcardsQuery()
+    {
+        return "SELECT * FROM Flashcards";
+    }
+}

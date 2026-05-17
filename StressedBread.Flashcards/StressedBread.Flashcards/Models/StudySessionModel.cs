86cbd0c3f4f4f7bc14091d4e90091e7975ba38a4
StressedBread <martinpolak02@gmail.com>
2026-05-17 13:16:13 +0200

    refactor(models): remove StudySessionModel class

--- StressedBread.Flashcards/StressedBread.Flashcards/Models/StudySessionModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StressedBread.Flashcards.Models;
-internal class StudySessionModel
-{
-    public int Id { get; set; }
-    public int Score { get; set; }
-    public DateTime SessionDate { get; set; }
-    public int StackId { get; set; }
-}
